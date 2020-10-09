d6df64e8bb26df45b524fd69ff7a030623e8a826
rakshit <rakshitsinha40@gmail.com>
2020-10-09 15:21:18 +0530

    JDBC sql working

--- Employee_Management_system.0/src/com/barclays/DataSQL.java
+++ Employee_Management_system.0/src/com/barclays/DataSQL.java
@@ -16,12 +16,7 @@ public class DataSQL implements Data{
                         "jdbc:mysql://localhost:3306/emp","root","Rac");
 
                 System.out.println("Connected to database");
-/*
-                Statement stmt=con.createStatement();
-                ResultSet rs=stmt.executeQuery("select * from Employee");
-                while(rs.next())
-                    System.out.println(rs.getString(1)+"  "+rs.getString(2)+"  "+rs.getString(3)+rs.getString(4));
-                //con.close();*/
+
             }
             catch (Exception e) {
                 System.out.println(e);
@@ -51,7 +46,7 @@ public class DataSQL implements Data{
 
             pst.executeUpdate();
             System.out.println("Record Added to database");
-            //con.close();
+
 
         }
         catch(Exception e)
@@ -71,7 +66,7 @@ public class DataSQL implements Data{
         ResultSet i=pst.executeQuery();
 
         System.out.println("Checking ");
-        if(i!=null) {
+        if(i.next()) {
             System.out.println("Checking true");
             return true;
         }else {
@@ -86,10 +81,12 @@ public class DataSQL implements Data{
         PreparedStatement pst=con.prepareStatement(q);
         pst.setString(1,Id);
         ResultSet rs=pst.executeQuery();
+        Emp em = null;
         while(rs.next())
-            System.out.println("ID :"+rs.getString(1)+" \n"+"Name :"+rs.getString(2)+" \n"+"Address :"+rs.getString(3)+"\n"+"Role :"+rs.getString(4));
-        return new Emp(Id,rs.getString(2),rs.getString(3),rs.getString(4));
-    }
+            em=new Emp(Id,rs.getString(2),rs.getString(3),rs.getString(4));
+        //System.out.println("Emp object created");
+        return em;
+        }
 
     public void update(String Id,Emp emp) throws SQLException {
         getConnection();
@@ -111,7 +108,7 @@ public class DataSQL implements Data{
             String q="delete from Employee where id=?";
             PreparedStatement pst=con.prepareStatement(q);
             pst.setString(1,Id);
-            ResultSet rs=pst.executeQuery();
+            pst.executeUpdate();
             System.out.println("record deleted");
 
     }
